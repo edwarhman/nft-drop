717561ef163d672f3a336cebebcd219fd76c1c97
edwarhman <edwarhman@gmail.com>
2022-02-21 15:44:28 -0400

    TASK-1: create SlimeBitToken contract

---
+++ contracts/SlimeBitToken.sol
@@ -0,0 +1,8 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SlimeBitToken is ERC721, Ownable {
+
+}
