ce293cc267aa68265d7cecf4494ab7911f080a07
edwarhman <edwarhman@gmail.com>
2022-02-22 19:42:09 -0400

    TASK-2: add comments to contract, change maxMintAmount name

--- contracts/SlimeBitToken.sol
+++ contracts/SlimeBitToken.sol
@@ -4,28 +4,45 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SlimeBitToken is ERC721, Ownable {
+	///@notice cost of each token
 	uint public cost = 0.01 ether;
 	string baseUri;
+	///@notice Uri that points to the img showed when the NFT are not revealed
 	string public notRevealedUri;
+	///@notice Indicate if the drop is paused (true) or not (false)
 	bool public paused = false;
+	///@notice Indicates if the drop has already been revealed 
 	bool public revealed = false;
+	///@notice The minted tokens total supply
 	uint public supply;
-	uint public maxSupply 1000;
+	///@notice Max amount of token permited to mint
+	uint public maxSupply = 1000;
+	///@notice Max amount of token permited to mint per transaction
+	uint public maxMintAmountPerTx = 10;
 
 
 	constructor (
 		string memory _name,
 		string memory _symbol,
+		string memory _baseUri,
+		string memory _notRevealedUri
 
-	) ERC721(_name, _symbol) Ownable() {}
+	) ERC721(_name, _symbol) Ownable() {
+		baseUri = _baseUri;
+		notRevealedUri = _notRevealedUri;
+	}
 
 	// public functions
 
+	///@notice Allows the sender to mint a new token if it's possible.
+	///@param _mintAmount Specify the amount of tokens to mind,
+	///cannot exceed the max mint amount permited by transaction
+	///@dev It rejects if doing the operation exceeds the maximum token amount
 	function mint(uint _mintAmount) public payable onlyOwner {
 		require(!paused);
 		require(_mintAmount > 0);
 		require(supply + _mintAmount <= maxSupply)
-		require(_mintAmount < maxMintAmount, "You cannot exceeds the max mint amount.");
+		require(_mintAmount < maxMintAmountPerTx, "You cannot exceeds the max mint amount.");
 		if(msg.sender == owner()) {
 			require(msg.value >= cost * _mintAmount);
 		}
@@ -37,6 +54,10 @@ contract SlimeBitToken is ERC721, Ownable {
 		supply += _mintAmount;
 	}
 
+
+	///@notice Gets all the tokens in the specified address wallet
+	///@param _owner Address that have the tokens
+	///@return An array of tokens IDs that specified owner have in his wallet 
 	function walletOfOwner (address _owner)
 	public
 	view
@@ -45,7 +66,7 @@ contract SlimeBitToken is ERC721, Ownable {
 		uint[] memory tokensIds = new uint[](ownerTokenCount);
 		uint gotTokens;
 		uint id = 1;
-
+		// iterate throgh id total to get all the owner tokens 
 		while(id <= supply && gotTokens < ownerTokenCount) {
 			if(ownerOf(id) == _owner) {
 				tokensIds[gotTokens] = id;
@@ -58,17 +79,22 @@ contract SlimeBitToken is ERC721, Ownable {
 
 	// only owner functions
 
+	///@notice reveal the smart contract
 	function reveal() public onlyOwner {
 		revealed = true;
 	}
 
+	///@notice set cost of tokens
+	///@param _newCost The new cost to set
 	function setCost(uint _newCost) public onlyOwner {
 			cost = _newCost;
 	}
 
+	///@notice set the maximun mint amount per transaction
 	function setMaxMintAmount(uint _newMaxMinAmount) public onlyOwner {
-		maxMintAmount = _newMaxMinAmount;
+		maxMintAmountPerTx = _newMaxMinAmount;
 	}
+
 
 	function setNotRevealedUri(string memory _notRevealedUri) public onlyOwner {
 		notRevealedUri = _notRevealedUri;
@@ -78,10 +104,13 @@ contract SlimeBitToken is ERC721, Ownable {
 		baseUri = _newBaseUri;
 	}
 
+	///@notice Set the pause state of the nft drop to true or false
+	///@param _newState A bool value that set the drop paused state   
 	function pause(bool _newState) public onlyOwner {
 		paused = _newState;
 	}
 
+	///@notice let the contract owner to withdraw the funds
 	function withdraw() public payable onlyOwner {
 		(bool os,) = payable(owner()).call{value : address(this).balance}("");
 	}
