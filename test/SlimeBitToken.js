95417bc4c529f3d4afe99cda645fbd7aefea4a3b
edwarhman <edwarhman@gmail.com>
2022-02-23 07:41:59 -0400

    TASK-9: add deployment arguments variables

--- test/SlimeBitToken.js
+++ test/SlimeBitToken.js
@@ -1,17 +1,27 @@
 const {expect} = require('chai');
 
 describe('Slime Token Contract', ()=> {
+  // contract interaction variables
   let Token, token, owner, addr1, addr2;
+  // deployment arguments variables
+  let name = "Slime Bit Token";
+  let symbol = "SBT";
+  let baseUri = "";
+  let notRevealedUri = "";
 
   beforeEach(async ()=> {
     Token = await ethers.getContractFactory('SlimeBitToken');
-    token = await Token.deploy("Slime Bit Token", "SBT", "", "");
+    token = await Token.deploy(name, symbol, baseUri, notRevealedUri);
     [owner, addr1, addr2, _] = await ethers.getSigners();
   });
 
   describe("Deployment", ()=> {
     it("should set the right owner", async ()=> {
       expect(await token.owner()).to.equal(owner.address);
+    });
+
+    it("Should set token name, symbol, base URI and not revealed URI property", async ()=> {
+      
     });
   });
 
