276d8c5a046fcf7b6d114c9e3fa562ab92d3f875
edwarhman <edwarhman@gmail.com>
2022-02-24 15:46:24 -0400

    TASK-24: add tokenUri implementation

--- contracts/SlimeBitToken.sol
+++ contracts/SlimeBitToken.sol
@@ -89,6 +89,29 @@ contract SlimeBitToken is ERC721, Ownable {
 		return tokensIds;
 	}
 
+	function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(revealed == false) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+
 	// only owner functions
 
 	///@notice reveal the smart contract
