67511c8493565176fc6de529f7da1bd3c26d5163
edwarhman <edwarhman@gmail.com>
2022-02-24 23:01:09 -0400

    TASK-29: set up deploy hardhat config for rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,14 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
 module.exports = {
   solidity: "0.8.1",
+  "network": {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/d0a8b4c2deb94e57841929475e0d240d",
+      accounts: [access.env.PRIVATE_KEY]
+    },
+  }
 };
