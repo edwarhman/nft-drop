004cfcb7d2f2e02ca31e09c10bb26ae4b6299f61
edwarhman <edwarhman@gmail.com>
2022-02-27 02:58:54 -0400

    TASK-57: add ERC20 Token transactions test cases

--- test/SlimeBitToken.js
+++ test/SlimeBitToken.js
@@ -3,7 +3,7 @@ const provider = waffle.provider;
 
 describe('Slime Token Contract', ()=> {
   // contract interaction variables
-  let Token, token, owner, addr1, addr2;
+  let Token, token, Coin, coin, owner, addr1, addr2;
   // deployment arguments variables
   let name = "Slime Bit Token";
   let symbol = "SBT";
@@ -12,26 +12,27 @@ describe('Slime Token Contract', ()=> {
 
   before(async ()=> {
     Token = await ethers.getContractFactory('SlimeBitToken');
+    Coin = await ethers.getContractFactory('ERC20Token');
   });
 
   beforeEach(async ()=> {
-    token = await Token.deploy(name, symbol, baseUri, notRevealedUri);
     [owner, addr1, addr2, _] = await ethers.getSigners();
+    token = await Token.deploy(name, symbol, baseUri, notRevealedUri, owner.address);
   });
 
-  xdescribe("Deployment", ()=> {
-    xit("should set the right owner", async ()=> {
+  describe("Deployment", ()=> {
+    it("should set the right owner", async ()=> {
       expect(await token.owner()).to.equal(owner.address);
     });
 
-    xit("Should set token name, symbol and not revealed URI property", async ()=> {
+    it("Should set token name, symbol and not revealed URI property", async ()=> {
       expect(await token.name()).to.equal(name);
       expect(await token.symbol()).to.equal(symbol);
       expect(await token.notRevealedUri()).to.equal(notRevealedUri);
     });
   });
 
-  xdescribe("Public functions", ()=> {
+  describe("Public functions", ()=> {
     describe("Mint function assertions", ()=> {
 
       beforeEach(async ()=> {
@@ -41,7 +42,7 @@ describe('Slime Token Contract', ()=> {
       it("Should mint a new Token", async ()=> {
         const mintAmount = 3;
         const initialSupply = await token.supply();
-        await token.mint(mintAmount);
+        await token.mint(mintAmount, false);
 
         expect(await token.ownerOf(1)).to.equal(owner.address);
         expect(await token.balanceOf(owner.address)).to.equal(mintAmount);
@@ -51,7 +52,7 @@ describe('Slime Token Contract', ()=> {
       describe("assertions when drop and presale are paused ", ()=> {
         it("Should not allow to  mint if the drop and presale are paused", async ()=> {
           await token.pause(true);
-          await expect(token.mint(3))
+          await expect(token.mint(3, false))
           .to
           .be
           .revertedWith("The token drop and the presale are close");
@@ -61,7 +62,7 @@ describe('Slime Token Contract', ()=> {
           await token.pause(true);
           await token.setWhitelistStatus(true);
           await 
-            expect(token.mint(1))
+            expect(token.mint(1, false))
             .to
             .be
             .revertedWith("The token drop is paused and you are not in the presale whitelist");
@@ -75,7 +76,7 @@ describe('Slime Token Contract', ()=> {
           
           
           await token.grantRole(minterHex, addr1.address);
-          await token.connect(addr1).mint(1, {value: mintCost});
+          await token.connect(addr1).mint(1, false, {value: mintCost});
 
           expect(await token.ownerOf(1)).to.equal(addr1.address);
           expect(await token.balanceOf(addr1.address)).to.equal(1);
@@ -84,14 +85,14 @@ describe('Slime Token Contract', ()=> {
       });
 
       it("Should require at least an amount of one token to mint", async ()=> {
-        await expect(token.mint(0))
+        await expect(token.mint(0, false))
         .to
         .be
         .revertedWith("You need to specify at least an amount of one token to mint");
       });
       
       it("Should not allow exceeding the maximum mint operations allowed per transaction", async ()=> {
-        await expect(token.mint(15))
+        await expect(token.mint(15, false))
         .to
         .be
         .revertedWith("You cannot exceeds the max mint amount.");
@@ -101,42 +102,84 @@ describe('Slime Token Contract', ()=> {
         const mintCost = await token.cost();
         let tx, result;
 
-        await expect(token.connect(addr1).mint(3))
+        await expect(token.connect(addr1).mint(3, false))
         .to
         .be
         .revertedWith("You have to pay the token price");
 
         // transaction response (may not be included in the blockchain yet
-        tx = await token.connect(addr1).mint(3, {value: mintCost.mul(3)});
+        tx = await token.connect(addr1).mint(3, false, {value: mintCost.mul(3)});
         // wait until the transaction is confirmed
         result = await tx.wait();
         expect(result.status).to.equal(1);
         expect(await provider.getBalance(token.address)).to.equal(mintCost.mul(3));
       });
 
+      describe("On payment with external token assertions", ()=> {
+        beforeEach(async ()=> {
+          coin = await Coin.deploy("fungible", "FNG");
+        });
+
+        it("Should not allow to mint if contract is not approved to transfer coins", async ()=> {
+          const costInCoin = await token.costInCoin();
+          await token.setERC20TokenAddress(coin.address);
+          await coin.transfer(addr1.address, costInCoin.mul(3));
+          await expect(token.connect(addr1).mint(1, true))
+          .to
+          .be
+          .revertedWith("ERC20: insufficient allowance");
+        });
+
+        it("Should not allow to mint if sender has not sufficient coins", async ()=> {
+          const costInCoin = await token.costInCoin();
+          await token.setERC20TokenAddress(coin.address);
+          await coin.connect(addr1).approve(token.address, costInCoin);
+          await expect(token.connect(addr1).mint(1, true))
+          .to
+          .be
+          .revertedWith("ERC20: transfer amount exceeds balance");
+        });
+
+        it("Should allow to mint using extenal coin", async ()=> {
+          const costInCoin = await token.costInCoin();
+          await token.setERC20TokenAddress(coin.address);
+          await coin.transfer(addr1.address, costInCoin.mul(3));
+          await coin.connect(addr1).approve(token.address, costInCoin);
+          await token.connect(addr1).mint(1, true);
+
+          expect(await token.ownerOf(1)).to.equal(addr1.address);
+          expect(await token.balanceOf(addr1.address)).to.equal(1);
+          expect(await token.supply()).to.equal(1);
+          expect(await coin.balanceOf(token.address))
+          .to
+          .equal(costInCoin);
+
+        })
+
+      })
     });
 
-    xdescribe("Wallet of Owner function assertions", ()=> {
+    describe("Wallet of Owner function assertions", ()=> {
 
-      xit("Should return the specified address wallet", async ()=> {
+      it("Should return the specified address wallet", async ()=> {
         const expectedIds = ['1', '2', '3', '4'];
         const mintCost = await token.cost();
         let wallet;
         
-        await token.connect(addr1).mint(4, {value: mintCost.mul(4)});
+        await token.connect(addr1).mint(4, false, {value: mintCost.mul(4)});
         wallet = await token.walletOfOwner(addr1.address)
         wallet = wallet.map(el => el.toString());
         // arrays are different pointers so we need to use deep
         expect(wallet).to.deep.equal(expectedIds);
       });
 
-      xit("Should return an empty array when sender is not a owner", async ()=> {
+      it("Should return an empty array when sender is not a owner", async ()=> {
         expect(await token.walletOfOwner(addr1.address)).to.deep.equal([]);
       });
       
     });
 
-    xdescribe("tokenURI function assertions", ()=> {
+    describe("tokenURI function assertions", ()=> {
       it("Should reject if the token ID does not exist", async ()=> {
           await expect(token.tokenURI(3))
           .to
@@ -145,14 +188,14 @@ describe('Slime Token Contract', ()=> {
       });
 
       it("Should return not revealed uri if revealed status is false", async ()=> {
-        await token.mint(1);
+        await token.mint(1, false);
         expect(await token.tokenURI(1))
         .to
         .equal(await token.notRevealedUri());
       });
 
       it("Should retrieve an empty string because tokenUri is empty", async ()=> {
-        await token.mint(1);
+        await token.mint(1, false);
         await token.reveal();
         expect(await token.tokenURI(1))
         .to
@@ -163,7 +206,7 @@ describe('Slime Token Contract', ()=> {
         let notBlankUri = "myUri/abc/";
         let baseExtension = await token.baseExtension();
         await token.setBaseUri(notBlankUri);
-        await token.mint(1);
+        await token.mint(1, false);
         await token.reveal();
 
         expect(await token.tokenURI(1))
@@ -172,7 +215,7 @@ describe('Slime Token Contract', ()=> {
       });
     });
 
-    xdescribe("Burn token function assertions", ()=> {
+    describe("Burn token function assertions", ()=> {
       it("Should not allow to burn tokens that do not exist or that do not belong to the sender", async ()=> {
         await
         expect(token.burn(1))
@@ -180,7 +223,7 @@ describe('Slime Token Contract', ()=> {
         .be
         .revertedWith("The specified token does not exist");
         
-        await token.mint(1);
+        await token.mint(1, false);
         await
           expect(token.connect(addr1).burn(1))
         .to
@@ -191,7 +234,7 @@ describe('Slime Token Contract', ()=> {
       it("Should burn a token", async ()=> {
         let prevBurnBalance, prevBurnSupply;
 
-        await token.mint(1);
+        await token.mint(1, false);
         prevBurnBalance = await token.balanceOf(owner.address);
         prevBurnSupply = await token.supply();
 
@@ -273,7 +316,7 @@ describe('Slime Token Contract', ()=> {
       let contractBalance;
       let newBalance;
 
-      await token.connect(addr1).mint(5, {value: mintCost.mul(5)});
+      await token.connect(addr1).mint(5, false, {value: mintCost.mul(5)});
       contractBalance = await provider.getBalance(token.address);
       tx = await token.withdraw();
       tx = await tx.wait();
@@ -283,10 +326,21 @@ describe('Slime Token Contract', ()=> {
       expect(await provider.getBalance(token.address)).to.equal(0);
     });
 
-    it("Should let the owner to set the whitelist status", async ()=> {
+    it("Should allow the owner to set the whitelist status", async ()=> {
       await token.setWhitelistStatus(true);
 
       expect(await token.whiteListActive()).to.equal(true);
+    })
+
+    it("Should allow set the external token contract address", async ()=> {
+      expect(await token.ERC20TokenAddress())
+      .to
+      .not
+      .equal(addr1.address);
+      await token.setERC20TokenAddress(addr1.address);
+      expect(await token.ERC20TokenAddress())
+      .to
+      .equal(addr1.address);
     })
   });
 
